97fb466ce4db802692bd4b49707a0eb98ab3fe36
David Gadling <dave@toasterwaffles.com>
2023-03-27 17:39:27 -0700

    Clean up a bunch
    1. There shouldn't be any more warnings
    2. We're actually counting PNG's now, not all files
    3. Along those lines, we're not recounting every time in the loop, we have a variable.
    4. Moved a bunch of things outside of the loop
    5. While we don't do anything about day rollover, we at least detect it
    6. Have a config file!

--- src/main.rs
+++ src/main.rs
@@ -1,74 +1,78 @@
 use chrono::{Datelike, Local};
-use image::{ImageBuffer, Rgba};
-use rusttype::{Font, Scale};
+// use image::{ImageBuffer, Rgba};
+// use rusttype::{Font, Scale};
 use screenshots::Screen;
 use serde::{Deserialize, Serialize};
 // use std::alloc::System;
+use env_logger::Builder;
+use log::{debug, LevelFilter};
 use std::fs;
 use std::fs::{create_dir_all, File};
 use std::thread;
-use std::time::{Duration, SystemTime};
+use std::time::Duration;
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Config {
     interval: u64,
-    max_sleep_secs: u64,
+    max_sleep_secs: u16,
     output_dir: String,
 }
 
 fn main() {
+    Builder::new().filter_level(LevelFilter::max()).init();
     let config_file = File::open("config.json").expect("Failed to open config.json");
     let config: Config = serde_json::from_reader(config_file).expect("Failed to read config file");
+    debug!("Read config of: {:?}", config);
 
-    let mut last_dir_date = String::new();
-    // let last_screenshot_time = SystemTime::now();
+    let starting_time = Local::now();
+    let mut last_time = starting_time.clone();
+    let year = starting_time.year().to_string();
+    let month = format!("{:02}", starting_time.month());
+    let day = format!("{:02}", starting_time.day());
+
+    let output_dir = std::path::Path::new(&config.output_dir)
+        .join(&year)
+        .join(&month)
+        .join(&day);
+
+    create_dir_all(&output_dir).expect("Failed to create output directory");
+    let mut curr_frame = get_curr_frame(&output_dir).expect("Failed to count files");
+    let screen = Screen::all().unwrap().first().unwrap().to_owned();
 
     loop {
         let now = Local::now();
-        let year = now.year().to_string();
-        let month = format!("{:02}", now.month());
-        let day = format!("{:02}", now.day());
-        let dir_date = format!("{}-{}-{}", year, month, day);
-
-        let output_dir = std::path::Path::new(&config.output_dir)
-            .join(&year)
-            .join(&month)
-            .join(&day);
-
-        if dir_date > last_dir_date {
-            create_dir_all(&output_dir).expect("Failed to create output directory");
-            last_dir_date = dir_date.clone();
+        if now.timestamp() - last_time.timestamp() > config.max_sleep_secs.into() {
+            // TODO: Handle the day rolling over, or needing a new directory to work in
+            panic!("Uhhh, day rolled over, I should do something smarter here!");
         }
 
-        let num_files = count_files(&output_dir).expect("Failed to count files");
-
-        let filename = format!("{:05}.png", num_files);
+        let filename = format!("{:05}.png", curr_frame);
         let filepath = output_dir.join(filename);
 
-        let buffer = Screen::all()
-            .unwrap()
-            .first()
-            .unwrap()
-            .capture()
-            .unwrap()
-            .buffer()
-            .to_owned();
+        let capture = screen.capture().unwrap();
 
-        fs::write(&filepath, buffer).expect("Failed to write PNG data to file");
-        print!("Presumably just wrote to {:?}", filepath);
+        fs::write(&filepath, capture.buffer()).expect("Failed to write PNG data to file");
 
+        curr_frame += 1;
+        last_time = now;
         thread::sleep(Duration::from_secs(config.interval));
     }
 }
 
-fn count_files(dir: &std::path::Path) -> std::io::Result<usize> {
+fn get_curr_frame(dir: &std::path::Path) -> std::io::Result<usize> {
+    debug!("Examining {:?}", dir);
     let mut count = 0;
     for entry in std::fs::read_dir(dir)? {
         let entry = entry?;
-        if entry.file_type()?.is_file() {
-            count += 1;
+        if !(entry.file_type()?.is_file()) {
+            continue;
         }
+        if !(entry.path().extension().unwrap() == "png") {
+            continue;
+        }
+        count += 1;
     }
+    debug!("Found {:?} existing PNGs", count);
     Ok(count)
 }
 
