bea8f5c1830b9e9be17358c5ff539bf3c462aa92
David Gadling <dave@toasterwaffles.com>
2023-03-28 12:15:41 -0700

    Filler frame creation
    Well, it gets created and it looks correctly centered.
    There's still a lot more to do.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use chrono::{Datelike, Local};
-// use image::{ImageBuffer, Rgba};
-// use rusttype::{Font, Scale};
+use image::{ImageBuffer, Rgba};
+use rusttype::{Font, Scale};
 use screenshots::Screen;
 use serde::{Deserialize, Serialize};
 // use std::alloc::System;
+use duration_human::DurationHuman;
 use env_logger::Builder;
-use log::{debug, LevelFilter};
+use log::{debug, info, LevelFilter, warn};
 use std::fs;
 use std::fs::{create_dir_all, File};
 use std::thread;
@@ -14,7 +15,7 @@ use std::time::Duration;
 #[derive(Debug, Serialize, Deserialize)]
 struct Config {
     interval: u64,
-    max_sleep_secs: u16,
+    max_sleep_secs: u64,
     output_dir: String,
 }
 
@@ -38,10 +39,13 @@ fn main() {
     create_dir_all(&output_dir).expect("Failed to create output directory");
     let mut curr_frame = get_curr_frame(&output_dir).expect("Failed to count files");
     let screen = Screen::all().unwrap().first().unwrap().to_owned();
+    let sleep_interval = Duration::from_secs(config.interval);
 
     loop {
         let now = Local::now();
-        if now.timestamp() - last_time.timestamp() > config.max_sleep_secs.into() {
+        let elapsed_since_last_shot = Duration::new((now.timestamp() - last_time.timestamp()) as u64, 0);
+        if elapsed_since_last_shot.as_secs() > config.max_sleep_secs {
+            create_filler_frame(elapsed_since_last_shot, 860, 360).save(output_dir.join("test-frame.png")).expect("Couldn't create filler frame!");
             // TODO: Handle the day rolling over, or needing a new directory to work in
             panic!("Uhhh, day rolled over, I should do something smarter here!");
         }
@@ -49,13 +53,19 @@ fn main() {
         let filename = format!("{:05}.png", curr_frame);
         let filepath = output_dir.join(filename);
 
-        let capture = screen.capture().unwrap();
+        let capture_result = screen.capture();
+        match capture_result {
+            Ok(capture) => {
+                fs::write(&filepath, capture.buffer()).expect("Failed to write PNG data to file");
+                curr_frame += 1;
+                last_time = now;
+            },
+            Err(error) => {
+                warn!("Trouble capturing screen: {:?}", error);
+            }
+        }
 
-        fs::write(&filepath, capture.buffer()).expect("Failed to write PNG data to file");
-
-        curr_frame += 1;
-        last_time = now;
-        thread::sleep(Duration::from_secs(config.interval));
+        thread::sleep(sleep_interval);
     }
 }
 
@@ -76,32 +86,28 @@ fn get_curr_frame(dir: &std::path::Path) -> std::io::Result<usize> {
     Ok(count)
 }
 
-/*
 fn create_filler_frame(
     duration: Duration,
-    width: Option<u32>,
-    height: Option<u32>,
-) -> image::DynamicImage {
-    let _width = width.unwrap_or(1920);
-    let _height = height.unwrap_or(1080);
+    width: u32,
+    height: u32,
+) -> ImageBuffer<Rgba<u8>, Vec<u8>> {
     let black = Rgba([0, 0, 0, 255]);
-    let img = ImageBuffer::from_pixel(_width, _height, black);
+    let white = Rgba([255, 255, 255, 255]);
+    let mut img = ImageBuffer::from_pixel(width, height, black);
 
-
-    let duration_str = format!("{:?}", duration);
+    let duration_str = format!("{:#} go by", DurationHuman::try_from(duration).unwrap());
     let font_data = include_bytes!("Ubuntu-Regular.ttf");
     let font = Font::try_from_bytes(font_data as &[u8]).unwrap();
-    let font_size = 120.0;
+    let font_size = 80.0;
     let scale = Scale::uniform(font_size);
-    let v_metrics = font.v_metrics(scale);
-    let offset_x = ((_width as f32 - font_size * duration_str.len() as f32) / 2.0) as f32;
-    let offset_y = ((_height as f32 - font_size) / 2.0 + v_metrics.ascent) as f32;
+    let (text_w, text_h) = imageproc::drawing::text_size(scale, &font, &duration_str);
+    let offset_x = (width as f32 / 2.0) - (text_w as f32 / 2.0);
+    let offset_y = (height as f32 / 2.0) - (text_h as f32 / 2.0);
 
     // Write the text to the image
-    let draw_text = imageproc::drawing::draw_text_mut;
-    draw_text(
+    imageproc::drawing::draw_text_mut(
         &mut img,
-        Rgba([255, 255, 255, 255]),
+        white,
         offset_x as i32,
         offset_y as i32,
         scale,
@@ -110,6 +116,5 @@ fn create_filler_frame(
     );
 
     // return the resulting image
-    img.
+    img
 }
-*/
