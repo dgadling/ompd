89a281483e472eb4bf4d03e3cc2584c118527e08
David Gadling <dave@toasterwaffles.com>
2023-04-22 20:56:55 -0700

    fix: capturer: Remove stray comments
    As we figured out, this isn't needed. So handy!
    Closes #1 as well

--- src/capturer/not_windows.rs
+++ src/capturer/not_windows.rs
@@ -2,11 +2,5 @@ use anyhow::Error;
 use screenshots::{Image, Screen};
 
 pub fn get_screenshot(screen: Screen) -> Result<Image, Error> {
-    /*
-    NOTE: On OS X we can use
-        /usr/bin/pmset -g systemstate | grep -q Graphics
-    to see if the display is on or not.
-    if that exits dirty, there's no graphics. We should just sleep & continue
-    */
     screen.capture()
 }
