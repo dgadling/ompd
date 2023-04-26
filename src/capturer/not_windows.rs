dbcc012d9b3db3f4bc20bedd0cc62e41036174c1
David Gadling <dave@toasterwaffles.com>
2023-04-26 16:20:58 -0700

    fix: Capturer: Do the simplest pmset check on Mac OS
    Since I don't have a Mac development environment I can't do the fancier
    version of this as mentioned in #1. So, for now, do the thing I can:
    the equivalent of `pmset -g systemstate | grep Graphics`

--- src/capturer/not_windows.rs
+++ src/capturer/not_windows.rs
@@ -1,6 +1,36 @@
-use anyhow::Error;
+use anyhow::{anyhow, Error};
 use screenshots::{Image, Screen};
 
+#[cfg(target_os = "macos")]
+use std::process::Command;
+
 pub fn get_screenshot(screen: Screen) -> Result<Image, Error> {
+    if !have_graphics() {
+        return Err(anyhow!("Don't appear to have graphics!"));
+    }
+
     screen.capture()
+}
+
+#[cfg(target_os = "macos")]
+fn have_graphics() -> bool {
+    let mut to_run = Command::new("/usr/bin/pmset");
+    to_run.args(["-g", "systemstate"]);
+
+    let output = to_run.output().expect("Failed to run pmset! :(");
+    let stdout_raw = String::from_utf8(output.stdout).unwrap();
+    let stdout = stdout_raw.lines().collect::<Vec<_>>();
+
+    for line in stdout {
+        if line.contains("Graphics") {
+            return true;
+        }
+    }
+
+    false
+}
+
+#[cfg(not(target_os = "macos"))]
+fn have_graphics() -> bool {
+    true
 }
